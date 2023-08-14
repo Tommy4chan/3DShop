b8c1b83e80d390f06eafe3ef6977bc976f7002a8
Tommy4chan <tommy4chan@gmail.com>
2023-08-14 22:21:25 +0300

    added logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
+    <link rel="icon" href="{{asset('logo.png')}}" type="image/x-icon">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
 </head>
